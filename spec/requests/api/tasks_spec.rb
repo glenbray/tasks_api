b83175155d0b24dc77e0215cc6080f1b8a94c3b9
Glen Bray <glen.bray@gmail.com>
2025-05-12 21:07:56 +1000

    setup rspec and tasks endpoint with a simple spec

---
+++ spec/requests/api/tasks_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Tasks API', type: :request do
+  describe 'GET /api/tasks' do
+    it 'returns a successful response' do
+      get '/api/tasks'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
