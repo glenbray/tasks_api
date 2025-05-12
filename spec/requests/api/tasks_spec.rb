623e8a7889ff6514ba80ae4716d7cd500b0a39de
Glen Bray <glen.bray@gmail.com>
2025-05-12 21:17:07 +1000

    run standardrb

--- spec/requests/api/tasks_spec.rb
+++ spec/requests/api/tasks_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Tasks API', type: :request do
-  describe 'GET /api/tasks' do
-    it 'returns a successful response' do
-      get '/api/tasks'
+RSpec.describe "Tasks API", type: :request do
+  describe "GET /api/tasks" do
+    it "returns a successful response" do
+      get "/api/tasks"
       expect(response).to have_http_status(:success)
     end
   end
